8751b135989029155ab47709beb266c82bc975f0
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-12-17 02:46:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
-
 #include <iostream>
 #include "lib.h"
-using namespace std;
-int main(){
-    int pos;
-    char a[10][20], f[1][20];
-    for(int i=0;i<10;i++){
-        cin >>a[i];
-    }
-    cin >> f[0];
 
-    if(research(a, f, pos)){
-        cout <<pos <<endl;
-    }else{
-        cout << "non presente" <<endl;
+using namespace std;
+
+int main(){
+   char a[10][20];
+    int i;
+    for(i = 0; i < 10; i++){
+        cin >> a[i];
     }
+    char b[20];
+    cin >> b;
+    listanomi(a,b);
   return 0;
 }
